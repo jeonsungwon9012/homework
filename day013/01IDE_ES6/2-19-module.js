2c5676e671e7c6314f687515eb7882639da422d2
jeonsungwon9012 <10218tjddnjs@naver.com>
2026-03-23 17:00:41 +0900

    day013

---
+++ day013/01IDE_ES6/2-19-module.js
@@ -0,0 +1,7 @@
+let base = 100;
+export const add = x => base + x;
+export   const multiply = x => base * x;
+const getBase = ()=>base;
+
+//export {add, multiply};
+export default getBase;
---
+++ day013/01IDE_ES6/2-20-main.js
@@ -0,0 +1,4 @@
+import getBase, { add } from './2-19-module.js';
+
+console.log(add(4));
+console.log(getBase)
